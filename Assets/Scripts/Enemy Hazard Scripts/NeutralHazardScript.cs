3320e68caaebbb6deaf51bd8045e84f2a3ea40d2
Duomi-GH <mymiyahara808@gmail.com>
2025-11-05 14:27:30 -1000

    Edited Level 3, created Level 4, added neutral hazards

---
+++ Assets/Scripts/Enemy Hazard Scripts/NeutralHazardScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeutralHazardScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player hit a hazard!");
+            PlayerFail player = other.GetComponent<PlayerFail>();
+            
+            if (player != null)
+            {
+                player.Fail();
+            }
+            
+        }
+        
+    }
+}
